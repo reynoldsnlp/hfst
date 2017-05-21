76048509d388e1454fc438b11b60eb7245bf9c92
eaxelson <erik.axelson@helsinki.fi>
2017-05-21 08:33:36 +0300

    Improve test script and add expected results for cases where xfst solution does not exist but the result from hfst script is checked.

--- test/tools/fsmbook-tests/test.sh
+++ test/tools/fsmbook-tests/test.sh
@@ -70,6 +70,10 @@ else
     if ! [ "$1" = "" ]; then
 	examples=$1
     fi
+    if [ "$examples" = "FinnishNumerals" ]; then
+	echo "FinnishNumerals depends on NumbersToNumerals, running tests for both"
+	examples="NumbersToNumerals FinnishNumerals"
+    fi
 fi
 
 if [ "$2" = "--python" ]; then
@@ -83,6 +87,7 @@ for example in $examples;
 do
     echo "Testing "$example"..."
 
+    # If flags must be eliminated when comparing the results
     if [ "$example" = "EsperantoAdjectives" -o \
         "$example" = "EsperantoNounsAdjectivesAndVerbs" -o \
         "$example" = "EsperantoNounsAndAdjectivesWithTags" -o \
@@ -93,57 +98,61 @@ do
     else
         compare_flags="-q"
     fi
-    
-    # (1) If xfst solution exists,
-    if ! [ "$example" = "FinnishNumerals" -o \
-        "$example" = "FinnishProsody" -o \
-        "$example" = "Palindromes" ]; then
 
-        # compile with xfst/foma, if needed..
-        if [ "$COMPILE_FROM_SCRATCH" = "true" ]; then
-            if [ "$example" = "FinnishOTProsody" -o \
-                "$example" = "Lingala" -o \
-                "$example" = "DateParser" -o \
-                "$example" = "YaleShooting" ]; then
-                if [ "$example" = "DateParser" ] ; then
-                    echo "  compiling with foma (result from xfst will not be equivalent"
-                    echo "  because symbols enclosed in square brackets are not harmonized)"
-                else
-                    echo "  compiling with foma (result from xfst will be too big)"
-                fi
-                if ! ($FOMA -f xfst-scripts/$example.xfst.script > /dev/null 2> LOG); then
-                    echo "ERROR: compiling with foma failed"
-                    cat LOG
-                    exit 1
-                fi
+    # Check if xfst solution exists
+    XFST_SOLUTION_EXISTS="true"
+    if [ "$example" = "FinnishNumerals" -o \
+	 "$example" = "FinnishProsody" -o \
+	 "$example" = "Palindromes" ]; then
+	XFST_SOLUTION_EXISTS="false";
+    fi
+
+    # (1) Generate or copy the expected result to file 'Expected_result'
+    if [ "$COMPILE_FROM_SCRATCH" = "true" -a "$XFST_SOLUTION_EXISTS" = "true" ]; then
+	# if foma must be used instead of xfst
+        if [ "$example" = "FinnishOTProsody" -o \
+			"$example" = "Lingala" -o \
+			"$example" = "DateParser" -o \
+			"$example" = "YaleShooting" ]; then
+            if [ "$example" = "DateParser" ] ; then
+                echo "  compiling with foma (result from xfst will not be equivalent"
+                echo "  because symbols enclosed in square brackets are not harmonized)"
             else
-                echo "  compiling with xfst.."
-                if ! ($XFST -f xfst-scripts/$example.xfst.script 2> LOG); then
-                    echo "ERROR: compiling with xfst failed"
-                    cat LOG
-                    exit 1;
-                fi
+                echo "  compiling with foma (result from xfst will be too big)"
+            fi
+            if ! ($FOMA -f xfst-scripts/$example.xfst.script > /dev/null 2> LOG); then
+                echo "ERROR: compiling with foma failed"
+                cat LOG
+                exit 1
             fi
         else
-            if ! [ -e expected-results/$example.prolog ]; then
-                echo "ERROR: file expected-results/"$example".prolog missing"
-                exit 1
-            else
-                cp expected-results/$example.prolog Result
+            echo "  compiling with xfst.."
+            if ! ($XFST -f xfst-scripts/$example.xfst.script 2> LOG); then
+                echo "ERROR: compiling with xfst failed"
+                cat LOG
+                exit 1;
             fi
         fi
-
-        # and convert from prolog to openfst-tropical for comparing.
-        cp Result Result.prolog
-        if ! (cat Result | $tooldir/hfst-txt2fst --prolog -f $common_format > tmp && \
-            mv tmp Result_from_xfst); then
-            echo "ERROR: in converting result from xfst/foma to hfst format"
-            exit 1;
+    else
+        if ! [ -e expected-results/$example.prolog ]; then
+            echo "ERROR: file expected-results/"$example".prolog missing"
+            exit 1
+        else
+            cp expected-results/$example.prolog Result
         fi
-	rm -f Result.prolog
-
-        # Also compile with hfst-xfst using all back-end formats..
-        if [ "$COMPILE_XFST_SCRIPT" == "true" ]; then
+    fi
+    # convert from prolog to openfst-tropical for comparing.
+    cp Result Result.prolog
+    if ! (cat Result | $tooldir/hfst-txt2fst --prolog -f $common_format > tmp && \
+		 mv tmp Expected_result); then
+        echo "ERROR: in converting result from xfst/foma to hfst format"
+        exit 1;
+    fi
+    rm -f Result.prolog
+    # The expected result is now in file 'Expected_result' in prolog format
+    
+    # (2) Compile with hfst-xfst or hfst python api using all back-end formats..
+    if [ "$COMPILE_XFST_SCRIPT" == "true" -a "$XFST_SOLUTION_EXISTS" = "true" ]; then
         for format in $backend_formats; 
         do
 	    if ! [ "$PYTHON" == "" ]; then
@@ -171,18 +180,18 @@ do
 	    fi
             # and convert from prolog to openfst-tropical and compare the results.
             if ! (cat Result | $tooldir/hfst-txt2fst --prolog -f $common_format > tmp && \
-                mv tmp Result_from_hfst_xfst); then
+			 mv tmp Result_from_hfst_xfst); then
                 echo "ERROR: in converting result from hfst prolog to binary format"
                 exit 1;
             fi
-            if ! ($tooldir/hfst-compare $compare_flags Result_from_xfst Result_from_hfst_xfst); then
+            if ! ($tooldir/hfst-compare $compare_flags Expected_result Result_from_hfst_xfst); then
                 echo "FAIL: results from xfst and hfst-xfst ("$format") are not equivalent, storing results in files:"
                 echo "    log/"$example.result_from_xfst_script_using_xfst_tool 
                 echo "    log/"$example.result_from_hfst_xfst_using_backend_format_$format
                 if ! [ -d log ]; then
                     mkdir log
                 fi
-                cp Result_from_xfst log/$example.result_from_xfst_script_using_xfst_tool
+                cp Expected_result log/$example.result_from_xfst_script_using_xfst_tool
                 cp Result_from_hfst_xfst log/$example.result_from_hfst_xfst_using_backend_format_$format
                 if [ "$EXIT_IF_NOT_EQUIVALENT" = "true" ]; then
                     exit 1;
@@ -190,11 +199,9 @@ do
             fi
 	    rm -f Result_from_hfst_xfst
         done
-        fi
-
     fi
 
-    # (2) Compile hfst script with all back-end formats and compare the results..
+    # (3) Compile hfst script with all back-end formats and compare the results..
     if [ "$COMPILE_HFST_SCRIPT" = "true" ]; then
     for format in $backend_formats; 
     do
@@ -244,107 +251,80 @@ do
         cat Result | $tooldir/hfst-fst2fst -f $common_format > tmp
         mv tmp Result_from_hfst_script_$format
         
-        # with the result from xfst/foma, if available..
-        if ! [ "$example" = "FinnishNumerals" -o \
-            "$example" = "FinnishProsody" -o \
-            "$example" = "Palindromes" ]; then
-            # special case 1
-            if [ "$example" = "EinsteinsPuzzle" ]; then
-		echo '(generating all word forms from xfst result, this can take a while)'
-                if ! ($tooldir/hfst-fst2strings --xfst=print-space Result_from_xfst | grep "German coffee Prince fish" > /dev/null); then
-                    echo "  FAIL: Results differ"
-                    if [ "$EXIT_IF_NOT_EQUIVALENT" = "true" ]; then
-                        exit 1;
-                    fi
-                fi
-		echo '(generating all word forms from hfst result, this can take a while)'
-                if ! ($tooldir/hfst-fst2strings Result_from_hfst_script_$format | \
-                      grep "fish" | grep "German coffee Prince fish" > /dev/null); then
-                    echo "  FAIL: Results differ"
-                    if [ "$EXIT_IF_NOT_EQUIVALENT" = "true" ]; then
-                        exit 1;
-                    fi
-                fi
-                continue
-            fi
-            # special case 2
-            if [ "$example" = "NumbersToNumerals" ]; then
-                if ! [ -d tmpdir ]; then
-                    mkdir tmpdir
-                fi
-                cp Result_from_xfst tmpdir/NumbersToNumerals # needed in FinnishNumerals
-                $tooldir/hfst-fst2strings Result_from_xfst | sort > tmp_xfst
-                $tooldir/hfst-fst2strings Result_from_hfst_script_$format | sort > tmp_hfst
-                if ! (diff tmp_xfst tmp_hfst); then
-                    echo "  FAIL: Results differ"
-                    if [ "$EXIT_IF_NOT_EQUIVALENT" = "true" ]; then
-                        exit 1;
-                    fi
-                fi
-                rm -f tmp_hfst tmp_xfst
-                continue
-            fi
-            if ! ($tooldir/hfst-compare $compare_flags Result_from_xfst Result_from_hfst_script_$format); then
-                echo "  FAIL: Results from xfst and hfst scripts ("$format") differ in test "$example", storing results in files:"
-                echo "    log/"$example.result_from_xfst_script_using_xfst_tool 
-                echo "    log/"$example.result_from_hfst_script_using_backend_format_$format
-                if ! [ -d log ]; then
-                    mkdir log
-                fi
-                cp Result_from_xfst log/$example.result_from_xfst_script_using_xfst_tool
-                cp Result_from_hfst_script_$format log/$example.result_from_hfst_script_using_backend_format_$format
+        # special case 1
+        if [ "$example" = "EinsteinsPuzzle" ]; then
+	    echo '(generating all word forms from xfst result, this can take a while)'
+            if ! ($tooldir/hfst-fst2strings --xfst=print-space Expected_result | grep "German coffee Prince fish" > /dev/null); then
+                echo "  FAIL: Results differ"
                 if [ "$EXIT_IF_NOT_EQUIVALENT" = "true" ]; then
                     exit 1;
                 fi
             fi
-            
-        # or with the result in common format.
-        else
-            # special case
-            if [ "$example" = "FinnishNumerals" -a "$format" = "sfst" ]; then
-                echo "    skipping comparing the results, result from SFST will be big because of epsilon handling in composition.."
-                continue
-            fi
-
-            if ! [ "$format" = "$common_format" ]; then
-                if ! ($tooldir/hfst-compare $compare_flags Result_from_hfst_script_$common_format Result_from_hfst_script_$format); then
-                    echo -n "  FAIL: Results from hfst scripts ("$format" and "$common_format") differ in test "$example
-                    echo ", storing results in files:"
-                    echo "    log/"$example.result_from_hfst_script_using_backend_format_$format
-                    echo "    log/"$example.result_from_hfst_script_using_backend_format_$common_format
-                    if ! [ -d log ]; then
-                        mkdir log
-                    fi
-                    cp Result_from_hfst_script_$common_format log/$example.result_from_hfst_script_using_backend_format_$common_format
-                    cp Result_from_hfst_script_$format log/$example.result_from_hfst_script_using_backend_format_$format
-                    if [ "$EXIT_IF_NOT_EQUIVALENT" = "true" ]; then
-                        exit 1;
-                    fi
+	    echo '(generating all word forms from hfst result, this can take a while)'
+            if ! ($tooldir/hfst-fst2strings Result_from_hfst_script_$format | \
+			 grep "fish" | grep "German coffee Prince fish" > /dev/null); then
+                echo "  FAIL: Results differ"
+                if [ "$EXIT_IF_NOT_EQUIVALENT" = "true" ]; then
+                    exit 1;
                 fi
             fi
+            continue
         fi
+        # special case 2
+        if [ "$example" = "NumbersToNumerals" ]; then
+            if ! [ -d tmpdir ]; then
+                mkdir tmpdir
+            fi
+            cp Expected_result tmpdir/NumbersToNumerals # needed in FinnishNumerals
+            $tooldir/hfst-fst2strings Expected_result | sort > tmp_xfst
+            $tooldir/hfst-fst2strings Result_from_hfst_script_$format | sort > tmp_hfst
+            if ! (diff tmp_xfst tmp_hfst); then
+                echo "  FAIL: Results differ"
+                if [ "$EXIT_IF_NOT_EQUIVALENT" = "true" ]; then
+                    exit 1;
+                fi
+            fi
+            rm -f tmp_hfst tmp_xfst
+            continue
+        fi
+	# special case 3
+        if [ "$example" = "FinnishNumerals" -a "$format" = "sfst" ]; then
+            echo "    skipping comparing the results, result from SFST will be big because of epsilon handling in composition.."
+            continue
+        fi
+
+        if ! ($tooldir/hfst-compare $compare_flags Expected_result Result_from_hfst_script_$format); then
+            echo "  FAIL: Results from xfst and hfst scripts ("$format") differ in test "$example", storing results in files:"
+            echo "    log/"$example.result_from_xfst_script_using_xfst_tool 
+            echo "    log/"$example.result_from_hfst_script_using_backend_format_$format
+            if ! [ -d log ]; then
+                mkdir log
+            fi
+            cp Result_from_xfst log/$example.result_from_xfst_script_using_xfst_tool
+            cp Result_from_hfst_script_$format log/$example.result_from_hfst_script_using_backend_format_$format
+            if [ "$EXIT_IF_NOT_EQUIVALENT" = "true" ]; then
+                exit 1;
+            fi
+        fi            
     done
     fi
 
 done
 
-rm -f Result_from_xfst Result
+rm -f Expected_result Result
 for format in $backend_formats;
 do 
     rm -f Result_from_hfst_script_$format
 done
 
-#rm -f tmpdir/NumbersToNumerals
-#if [ -d "tmpdir" ]; then
-#    rmdir tmpdir
-#fi
+rm -f tmpdir/NumbersToNumerals
+if [ -d "tmpdir" ]; then
+    rmdir tmpdir
+fi
 
 echo ""
 echo "**********"
 echo "All fsmbook tests that were performed passed."
-#echo "Returning a skip value because result from DateParser test is compared with result from foma"
-#echo "instead of xfst, because foma and xfst handle symbols that are enclosed in square brackets"
-#echo "differently."
 echo "**********"
 echo ""
-#exit 77
+
