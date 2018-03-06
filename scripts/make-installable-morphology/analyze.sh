c02ab2df35d95744572e3d0d31c5e92aacd0850c
Sam Hardwick <sam.hardwick@iki.fi>
2018-03-06 16:10:25 +0200

    Check for programs and give info if missing

--- scripts/make-installable-morphology/analyze.sh
+++ scripts/make-installable-morphology/analyze.sh
@@ -8,6 +8,16 @@ if [ ! -e "$trdir"/LANGCODE-analysis.hfst.ol ] ; then
     echo "$trdir/LANGCODE-analysis.hfst.ol not found"
     exit 1
 fi
+if command -v hfst-tokenize 2 > /dev/null; then
+    echo "hfst-tokenize not found on your system"
+    echo "HFST is available from https://github.com/hfst/hfst"
+    exit 1
+fi
+if command -v hfst-lookup 2 > /dev/null; then
+    echo "hfst-lookup not found on your system"
+    echo "HFST is available from https://github.com/hfst/hfst"
+    exit 1
+fi
 if [ "$1" == "--help" ] ; then
     echo "Provides a morphological analysis of text from standard input."
     echo "Input can be running text, in which case it is tokenized first."
@@ -15,5 +25,5 @@ if [ "$1" == "--help" ] ; then
 fi
 
 hfst-tokenize $trdir/LANGCODE-tokenize.pmatch |
-    hfst-lookup -p -b 25 $trdir/LANGCODE-analysis.hfst.ol |
+    hfst-lookup -p $trdir/LANGCODE-analysis.hfst.ol |
     cut -f1,2
--- scripts/make-installable-morphology/generate.sh
+++ scripts/make-installable-morphology/generate.sh
@@ -8,6 +8,11 @@ if [ ! -e "$trdir"/LANGCODE-generation.hfst.ol ] ; then
     echo "$trdir/LANGCODE-generation.hfst.ol not found"
     exit 1
 fi
+if command -v hfst-lookup 2 > /dev/null; then
+    echo "hfst-lookup not found on your system."
+    echo "HFST is available from https://github.com/hfst/hfst."
+    exit 1
+fi
 if [ "$1" == "--help" ] ; then
     echo "Generates surface forms from morphological analyses from standard input."
     echo "One analysis per line."
