368f15b9e8e8ac316aac42bec33498e05202015c
Sam Hardwick <sam.hardwick@iki.fi>
2018-12-17 16:24:23 +0200

    Sort PROPtags so they appear in consistent order

--- scripts/finnish-tagtools/tag/finnpos-restore-lemma.py
+++ scripts/finnish-tagtools/tag/finnpos-restore-lemma.py
@@ -1,35 +1,40 @@
 #! /usr/bin/env python3
 
-from sys import stdin, stdout, argv
+from sys import stdin, stdout, stderr, argv
 
-for_finer = False
-if len(argv) > 1 and argv[1] == "--finer":
-    for_finer = True
+HASH="<HASH>"
+for_ner = False
 
-def part_count(lemma):
-    return lemma.count('#')
-
-def compile_dict(label_lemma_pairs):
-    res = {}
-
-    for label, lemma in label_lemma_pairs:
-        if label in res:
-            old_lemma = res[label]
-
-            if part_count(old_lemma) > part_count(lemma):
-                res[label] = lemma
-        else:
-            res[label] = lemma
-
-    return res
-
-def get_proptags(label2lemma):
+def get_proptags(label_lemma_pairs):
     proptags = set()
-    for label in label2lemma.keys():
+    for label, lemma in label_lemma_pairs:
         for part in label.split("|"):
             if part.startswith("[PROP="):
                 proptags.add(part)
-    return ''.join(list(proptags))
+    if len(proptags) != 0:
+        return ''.join(sorted(proptags))
+    return "_"
+
+def is_subset(tags1, tags2):
+    parts1 = tags1.split("|")
+    parts2 = tags2.split("|")
+    for part in parts1:
+        if part not in parts2:
+            return False
+    return True
+
+def is_exact_match(tags1, tags2):
+    parts1 = tags1.split("|")
+    parts2 = tags2.split("|")
+    if len(tags1) != len(tags2):
+        return False
+    for part in parts1:
+        if part not in parts2:
+            return False
+    return True
+
+if "ner" in argv:
+    for_ner = True
 
 for line in stdin:
     line = line.strip()
@@ -44,26 +49,28 @@ for line in stdin:
         if ann.find(' ') != -1:
             lemmas = ann[:ann.find(' ')]
             ann = ann[ann.find(' ') + 1:]
-        else:
-            ann = '_'
-        
-        lemma_dict = {}
+
+        ann = "_"
         if lemmas != '_':
-            lemma_dict = compile_dict(eval(lemmas))
-
-        if for_finer and "PROPER" in label:
-            PROPtags = get_proptags(lemma_dict)
-            if PROPtags != '':
-                ann = PROPtags
-        
-        if label in lemma_dict:
-            lemma = lemma_dict[label]
+            lemmas = eval(lemmas)
+            lemma_candidate = None
+            
+            for this_label, this_lemma in lemmas:
+                if is_exact_match(label, this_label):
+                    lemma = this_lemma
+                    lemma_candidate = None
+                    break
+                elif lemma_candidate is None and is_subset(label, this_label):
+                    lemma_candidate = this_lemma
+            if lemma_candidate:
+                lemma = lemma_candidate
+            elif '[PROPER=PROPER]' in lemmas[0][0]:
+                label = lemmas[0][0]
+                lemma = lemmas[0][1]
+            if '[PROPER=PROPER]' in label:
+                ann = get_proptags(lemmas)
+        lemma = lemma.replace(HASH, "#")
+        if for_ner:
             lemma = lemma.lower()
-            lemma = lemma.replace('#','')
-
-        if for_finer:
-            print('%s\t%s\t%s\t%s' % (wf, lemma,
-                                      label.replace('|',''),
-                                      ann.replace('|', '')))
-        else:
-            print('%s\t%s\t%s\t%s\t%s' % (wf, feats, lemma, label, ann))
+            label = label.replace('|', '')
+        print('%s\t%s\t%s\t%s\t%s' % (wf, feats, lemma, label, ann))
