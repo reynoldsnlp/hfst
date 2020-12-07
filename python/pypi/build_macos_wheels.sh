4c598cf9373f3db4b8c3faa8eab6d59d2b76eb71
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2020-12-07 15:41:10 -0700

    add tests to MacOS wheel script

--- python/pypi/build_macos_wheels.sh
+++ python/pypi/build_macos_wheels.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-echo "Configuring..."
+set -e -u -x
+
+echo "STEP Configuring..."
 cd ../../
 make clean
 autoreconf -i  # Is this necessary?
 ./configure --enable-all-tools --with-readline --enable-fsmbook-tests
-echo "Making flex/yacc files..."
+echo "STEP Making flex/yacc files..."
 cd libhfst/src/parsers/
 make
-echo "Building libhfst.py..."
+echo "STEP Building libhfst.py..."
 cd ../../../python/
 python3 setup.py build_ext --inplace
-echo "Copying cpp and other files..."
+echo "STEP Copying cpp and other files..."
 cd pypi/
 ./copy_files.sh
-echo "Building wheels..."
+echo "STEP Building binary distribution wheels..."
 for p in python35 python36 python37 python38 python39; do
 	$p setup.py build_ext --inplace
 	$p setup.py bdist_wheel
 done
 
+echo "STEP Building source distribution..."
 $p setup.py sdist
+
+echo "STEP Running tests..."
+cd ../
+for p in python35 python36 python37 python38 python39; do
+	$p -m pip install hfst --no-index -f pypi/dist/
+	$(cd test && ./test.sh --python $p)
+done
