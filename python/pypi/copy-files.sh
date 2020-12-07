002789f0156f4d08fa7e60c74f6a1f72291efc68
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2020-12-07 14:25:55 -0700

    add libhfst.py to copy-files.sh

--- python/pypi/copy-files.sh
+++ python/pypi/copy-files.sh
@@ -29,11 +29,9 @@ if ! [ -d "hfst" ]; then mkdir hfst; fi
 cp -R ../../back-ends/* back-ends/
 cp -R ../../libhfst/* libhfst/
 cp -R ../hfst/* hfst/
+cp ../*.cpp .
 
-for file in hfst_extensions.cpp hfst_file_extensions.cpp hfst_lexc_extensions.cpp \
-hfst_lookup_extensions.cpp hfst_pmatch_extensions.cpp hfst_pmatch_tokenize_extensions.cpp \
-hfst_prolog_extensions.cpp hfst_regex_extensions.cpp hfst_rules_extensions.cpp \
-hfst_xfst_extensions.cpp hfst_sfst_extensions.cpp libhfst_wrap.cpp libhfst.i docstrings.i ;
+for file in libhfst.i libhfst.py docstrings.i ;
 do
     cp ../$file $file
 done
